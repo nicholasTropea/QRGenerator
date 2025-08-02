68509fe204a65be80fcb83e197c78aaac2c9a53d
nicholasTropea <nicholastropea042@gmail.com>
2025-08-02 09:17:57 +0200

    Changed Home page design.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,22 @@
 import Button from '../components/Button.jsx';
 import InputField from '../components/InputField.jsx';
+import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
-    <>
-      <h1>Welcome to the QR Code Generator!</h1>
-      <p>
+    <div className={styles.main}>
+      <h1 className={styles.title}>Welcome to the QR Code Generator!</h1>
+
+      <p className={styles.infoText}>
         This tool can generate a QR code for anything you want: text, numbers, website URLs, even kanji characters!
         To see an in-depth list of what you can and cannot generate as a QR code, check the <a>what can I encode?</a> page! 
       </p>
-      <InputField placeHolder={"Place your input here!"} />
+      
+      <InputField classes={styles.inputField} placeHolder={"Place your input here!"} />
+
       <Button func={() => console.log("Pressed")}>
         Generate QR code!
       </Button>
-    </>
+    </div>
   );
 }
