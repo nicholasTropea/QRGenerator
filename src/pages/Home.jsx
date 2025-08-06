7782d2f5ba69fa5b7e36b02b88e7b1e34c4b37aa
nicholasTropea <nicholastropea042@gmail.com>
2025-08-06 19:37:06 +0200

    Added typing animation to title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from '../components/Button.jsx';
 import InputField from '../components/InputField.jsx';
 import Canvas from '../components/Canvas.jsx';
@@ -6,12 +6,57 @@ import styles from '../styles/Home.module.scss';
 import main from '../utils/main.js';
 
 export default function Home() {
+  const [titleVal, setTitle] = useState('');
   const [value, setValue] = useState('');
-
   const [matrix, setMatrix] = useState(null);
 
+  const phrases = [
+    "your website",
+    "an image",
+    "your text",
+    "a pdf",
+    "your WIFI password"
+  ];
+
+  useEffect(() => {
+    let phraseIndex = 0;
+    let charIndex = 0;
+    let typing = true;
+
+    const type = () => {
+      const currentPhrase = phrases[phraseIndex];
+
+      if (typing) {
+        setTitle(currentPhrase.slice(0, charIndex++ + 1));
+
+        // Finished writing phrase
+        if (charIndex === currentPhrase.length + 1) {
+          typing = false;
+          setTimeout(type, 2000);
+          return;
+        }
+      }
+      else {
+        setTitle(currentPhrase.slice(0, charIndex-- - 1));
+
+        // Finished deleting phrase
+        if (charIndex === 0) {
+          typing = true;
+          phraseIndex = (phraseIndex + 1) % phrases.length;
+        }
+      }
+
+      setTimeout(type, typing ? 100 : 80);
+    };
+
+    type();
+
+    // Cleanup in case component unmounts
+    return () => clearTimeout();
+  }, []);
+
   const handleMatrixGeneration = (value) => {
-    if (value === null || value === '' || value === undefined) return;
+    if (!value) return;
     
     const resultMatrix = main(value);
     setMatrix(resultMatrix);
@@ -22,9 +67,15 @@ export default function Home() {
     setValue('');
   }
 
+  const switchInput = () => {
+    
+    
+    let chars = Array.from(titleVal);
+  }
+
   return (
     <div className={styles.main}>
-      <h1 className={styles.title}>Welcome to the QR Code Generator!</h1>
+      <h1 className={styles.title}>Generate a QR code for {titleVal} <span id={styles.textBar}>|</span> </h1>
 
       <p className={styles.infoText}>
         This tool can generate a QR code for anything you want: text, numbers, website URLs, even kanji characters!
