ab1e508d30a2a429bd0acaa3f92a18bb5a342ecd
nicholasTropea <nicholastropea042@gmail.com>
2025-08-09 08:08:07 +0200

    Removed no longer needed useEffect import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import Overlay from '../components/Overlay.jsx';
 import HomeTitle from '../components/HomeTitle.jsx';
